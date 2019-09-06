2b82834b7826ac033731fbc2fbd742148f103643
WillianDomingues79 <willian.souza79@hotmail.com>
2019-09-05 23:53:14 -0300

    Códigos de movimentação, Contagem e Spawn

---
+++ Script/movimentarCarro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentarCarro : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float accelerationPower = 2f;
+    [SerializeField]
+    float steeringPower = 0.5f;
+    float steeringAmount = 2, speed, direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        steeringAmount = -Input.GetAxis("Horizontal");
+        speed = Input.GetAxis("Vertical") * accelerationPower;
+
+        direction = Mathf.Sign(Vector2.Dot (rb.velocity, rb.GetRelativeVector(Vector2.up)));
+        rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;
+
+        rb.AddRelativeForce(Vector2.up * speed);
+
+        rb.AddRelativeForce( - Vector2.right * rb.velocity.magnitude * steeringAmount / 6);
+    }
+}
---
+++ Script/player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    
+    //private Rigidbody2D playerRB;
+    //public float velocidade;
+
+
+    //private SpriteRenderer playerSprite; //mudar lado
+    //public bool flipX; //mudar lado
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        //playerRB = GetComponent<Rigidbody2D>();
+        //playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            velocidade = velocidade * -1;//vai para trás
+            //flipX = !flipX; //mudar lado
+            //playerSprite.flipX = flipX; //mudar lado
+
+        }
+        playerRB.velocity = new Vector2(velocidade, playerRB.velocity.y);//Vai para frente*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.tag == "bateu")
+        {
+            SceneManager.LoadScene("GameOver");
+            print("Bateu!");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D colisao)
+    {
+        if(colisao.gameObject.tag == "estacionou")
+        {
+            pontuacao.pontos += 1;
+            print("Estacionou!");
+        }
+    }
+
+    public void voltou()
+    {
+        SceneManager.LoadScene("SampleScene");
+        print("Bateu!");
+    }
+}
+
